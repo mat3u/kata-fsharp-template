a7fe6cd082af75fdf391cb22f7d0f40c86691a3f
Matt Stasch <matt.stasch@gmail.com>
2015-02-24 11:31:31 +0100

    CLI runner for non-windows platforms added

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -d "./src/packages/FAKE.3.17.7" ]; then
+    echo Restoring FAKE
+    mono ./src/.nuget/nuget.exe restore "./src/.nuget/packages.config" -SolutionDirectory "./src"
+fi
+
+mono ./src/packages/FAKE.3.17.7/tools/FAKE.exe %*
