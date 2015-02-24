33c5be384944476fdd25c5834c141ae7b69d9a2a
Matt Stasch <matt.stasch@gmail.com>
2015-02-24 11:57:28 +0100

    Backslash replaced with slash for multiplatform compatibility

--- build.fsx
+++ build.fsx
@@ -1,28 +1,28 @@
-#r @"src\packages\FAKE.Core.3.17.7\tools\FakeLib.dll"
+#r @"src/packages/FAKE.Core.3.17.7/tools/FakeLib.dll"
 
 open Fake
 
 Target "Clean" (fun _ ->
-    CleanDir @"src\bin"
+    CleanDir @"src/bin"
 )
 
 Target "RestorePackages" (fun _ ->
-    !! @"src\*.sln"
+    !! @"src/*.sln"
     |> Seq.iter (fun s ->
         RestoreMSSolutionPackages (fun p -> 
-            {p with OutputPath = @"src\packages"}
+            {p with OutputPath = @"src/packages"}
         ) s    
     )
 )
 
 Target "Build" (fun _ ->
-    !! @"src\**\*.fsproj"
-    |> MSBuild @"src\bin" "Build" []
+    !! @"src/**/*.fsproj"
+    |> MSBuild @"src/bin" "Build" []
     |> Log "Build log:"
 )
 
 Target "Test" (fun _ ->
-    !! @"src\bin\*.dll"
+    !! @"src/bin/*.dll"
     |> NUnit (fun p -> { p with DisableShadowCopy = true })
 )
 
