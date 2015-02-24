603dbf4232c8f3ba932bbb9123b563482deb13c3
Matt Stasch <matt.stasch@gmail.com>
2015-02-24 11:25:43 +0100

    Building from CLI

---
+++ run.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+if not exist ".\src\packages\FAKE.3.17.7" (
+    echo Restoring FAKE
+    .\src\.nuget\nuget.exe restore ".\src\.nuget\packages.config" -SolutionDirectory ".\src"
+)
+
+src.\packages.\FAKE.3.17.7\tools\FAKE.exe %*
